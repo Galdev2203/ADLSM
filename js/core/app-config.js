29d60695478641bbfc3aff7c349194db73cff548
Galdev2203 <guillealdev@gmail.com>
2026-09-22 01:18:48 +0200

    feat: centralize application configuration and version

---
+++ js/core/app-config.js
@@ -0,0 +1,22 @@
+export const APP_CONFIG = {
+  name: 'ADLSM',
+  fullName: 'ADLSM · Herramientas internas',
+  version: '0.17.0',
+  build: '2026.09.22.09',
+  season: '2026/27',
+  fab: {
+    schedulesUrl: 'https://fabasket.com/horarios/',
+    resultsPageUrl: 'https://fabasket.com/resultados/',
+    competitions: {
+      aragon: 'https://competiciones.feb.es/autonomicas/?a=3&c=23460&med=0',
+      zaragoza: 'https://competiciones.feb.es/autonomicas/?a=32&c=23063&med=0'
+    }
+  },
+  routes: {
+    home: './',
+    horarios: 'horarios/',
+    resultados: 'resultados/'
+  }
+};
+
+export const APP_VERSION_LABEL = `v${APP_CONFIG.version} · build ${APP_CONFIG.build}`;
