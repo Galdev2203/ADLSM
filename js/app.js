192f13894460c92a10e2d8e8b04cc259d0713745
Galdev2203 <guillealdev@gmail.com>
2026-09-22 01:03:23 +0200

    Bump parser cache version after PDF parser fix

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-import { findFabDocuments } from './fab/fab-source.js?v=20260922-05';
-import { parsePdfFile } from './fab/fab-parser.js?v=20260922-05';
+import { findFabDocuments } from './fab/fab-source.js?v=20260922-06';
+import { parsePdfFile } from './fab/fab-parser.js?v=20260922-06';
 import { normalizeTeamName } from './fab/fab-normalizer.js';
 import { renderMatches } from './ui/renderer.js';
 import { createTemplateCanvas, getTemplatePages } from './ui/instagram-template.js';
