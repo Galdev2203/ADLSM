6e28dba3670ef9a7f2a13d5392137b72af2bd330
Galdev2203 <guillealdev@gmail.com>
2026-09-22 09:29:49 +0200

    chore: bump app version for resultados selector

--- js/core/app-config.js
+++ js/core/app-config.js
@@ -1,15 +1,15 @@
 export const APP_CONFIG = {
   name: 'ADLSM',
   fullName: 'ADLSM · Herramientas internas',
-  version: '0.18.0',
-  build: '2026.09.22.10',
+  version: '0.19.0',
+  build: '2026.09.22.11',
   season: '2026/27',
   fab: {
     schedulesUrl: 'https://fabasket.com/horarios/',
     resultsPageUrl: 'https://fabasket.com/resultados/',
     competitions: {
-      aragon: 'https://competiciones.feb.es/autonomicas/?a=3&c=23460&med=0',
-      zaragoza: 'https://competiciones.feb.es/autonomicas/?a=32&c=23063&med=0'
+      aragon: 'https://competiciones.feb.es/autonomicas/?a=3',
+      zaragoza: 'https://competiciones.feb.es/autonomicas/?a=32'
     }
   },
   routes: {
