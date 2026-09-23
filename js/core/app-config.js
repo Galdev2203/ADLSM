dde5b824dde1e4d43637c2435928e0971649f979
Galdev2203 <guillealdev@gmail.com>
2026-09-23 14:35:17 +0200

    chore: bump internal application version

--- js/core/app-config.js
+++ js/core/app-config.js
@@ -1,9 +1,9 @@
 export const APP_CONFIG = {
   name: 'ADLSM',
   fullName: 'ADLSM · Herramientas internas',
-  version: '0.20.0',
-  build: '2026.09.22.17',
-  season: '2026/27',
+  version: '0.22.0',
+  build: '2026.09.23.01',
+  season: '',
   fab: {
     schedulesUrl: 'https://fabasket.com/horarios/',
     resultsPageUrl: 'https://fabasket.com/resultados/',
@@ -12,11 +12,7 @@ export const APP_CONFIG = {
       zaragoza: 'https://competiciones.feb.es/autonomicas/?a=32'
     }
   },
-  routes: {
-    home: './',
-    horarios: 'horarios/',
-    resultados: 'resultados/'
-  }
+  routes: { home: './', horarios: 'horarios/', resultados: 'resultados/' }
 };
 
 export const APP_VERSION_LABEL = `v${APP_CONFIG.version} · build ${APP_CONFIG.build}`;
